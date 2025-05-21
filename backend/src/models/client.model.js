d0b588a008289d4a2f19939a800d5b3fb82cd589
rohit <rohitmahto062@gmail.com>
2025-05-21 17:25:02 +0530

    feat: add client and receiver client models with schema definitions for MongoDB

---
+++ backend/src/models/client.model.js
@@ -0,0 +1,27 @@
+
+import mongoose, { Schema } from "mongoose";
+
+const clientSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    gstNumber: {
+        type: String,
+        required: true
+    },
+    reciverClient: {
+        type: Schema.Types.ObjectId,
+        ref: 'reciverClient'
+    }
+})
+
+export const Client = mongoose.model('Client', clientSchema);
---
+++ backend/src/models/reciverClient.model.js
@@ -0,0 +1,29 @@
+/*
+  _id string pk
+  fullName string
+  phoneNumber number
+  address string
+  gstNumber string
+*/
+import mongoose, { Schema } from "mongoose";
+
+const reciverClientSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    gstNumber: {
+        type: String,
+        required: true
+    }
+})
+
+export const ReciverClient = mongoose.model('ReciverClient', reciverClientSchema);
